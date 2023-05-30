ec67dc9dd809598bad6caf2bb66fda87d6c22373
RobD01 <medbz2k3@gmail.com>
2023-05-29 19:38:44 -0700

    form

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,4 +1,4 @@
-import NextAuth from "next-auth/next";
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 import User from "@models/user";
@@ -7,35 +7,40 @@ import { connectToDB } from "@utils/database";
 const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.clientId,
-      clientSecret: process.env.clientSecret,
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  callbacks: {},
-  async session({ session }) {
-    const sessionUser = await User.findOne({
-      email: session.User.email,
-    });
-    session.user.id = sessionUser._id.toString();
-    return session;
-  },
-  async signIn({ profile }) {
-    try {
-      await connectToDB();
-      // check if user already exists.
-      const userExists = await User.findOne({ email: profile.email });
+  callbacks: {
+    async session({ session }) {
+      // store the user id from MongoDB to session
+      const sessionUser = await User.findOne({ email: session.user.email });
+      session.user.id = sessionUser._id.toString();
 
-      // If not, create new user
-      await User.create({
-        email: profile.email,
-        image: profile.picture,
-      });
+      return session;
+    },
+    async signIn({ account, profile, user, credentials }) {
+      try {
+        await connectToDB();
 
-      return true;
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
+        // check if user already exists
+        const userExists = await User.findOne({ email: profile.email });
+
+        // if not, create a new document and save user in MongoDB
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+
+            image: profile.picture,
+          });
+        }
+
+        return true;
+      } catch (error) {
+        console.log("Error checking if user exists: ", error.message);
+        return false;
+      }
+    },
   },
 });
 
--- components/Form.jsx
+++ components/Form.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+import Link from "next/link";
 
-const Form = () => {
-  return <div>Form</div>;
+const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
+  return (
+    <section className="w-full max-w-full flex-start flex-col">
+      <h1 className="head_text text-left">
+        <span className="blue_gradient">{type} Post</span>
+      </h1>
+      <p className="desc text-left max-w-md">
+        {type} and share amazing prompts with the AI Platform
+      </p>
+
+      <form
+        onSubmit={handleSubmit}
+        className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism"
+      >
+        <label>
+          <span className="font-satoshi font-semibold text-base text-gray-700">
+            Your AI Prompt
+          </span>
+          <textarea
+            placeholder="Write your prompt here"
+            className="form_textarea"
+            value={post.prompt}
+            onChange={(e) => setPost({ ...post, prompt: e.target.value })}
+          />
+        </label>
+
+        <label>
+          <span className="font-satoshi font-semibold text-base text-gray-700">
+            Tag {`    `}{" "}
+            <span className="font-normal"> #product, #food, #business</span>
+          </span>
+          <input
+            placeholder="List the tags here"
+            className="form_input"
+            required
+            value={post.tag}
+            onChange={(e) => setPost({ ...post, tag: e.target.value })}
+          />
+        </label>
+      </form>
+    </section>
+  );
 };
 
 export default Form;
